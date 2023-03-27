0b92a75eab16066b078e070593f8e6dea1b0f751
Panisara Sonkom <[psonkom342@gmail.com]>
2023-03-27 15:11:24 +1100

    completed mobile nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import Navbar from "./components/Navbar"
+import DotGroup from "./components/DotGroup"
 import { useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery"
 
 function App() {
+
   // Determine which page of the webpage we're on
   const [selectedPage, setSelectedPage] = useState('home')
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
   // Check if user screen size is bigger or smaller than 1060px
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
 
+  //Check if scroll or not
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true)
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return <div className="app bg-white">
-    <Navbar selectedPage= {selectedPage} setSelectedPage= {selectedPage}/>
+    <Navbar
+      isTopOfPage = {isTopOfPage}
+      selectedPage= {selectedPage}
+      setSelectedPage= {selectedPage}
+    />
+    <div className= "w-5/6 mx-auto md:h-full">
+      {isAboveMediumScreens && (
+        <DotGroup
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
+    </div>
   </div>
 
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,33 @@ import {useState} from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import useMediaQuery from "../hooks/useMediaQuery"
 
+
 const Link = ({ page, selectedPage, setSelectedPage}) => {
     const lowerCasePage = page.toLowerCase()
     return (
         <AnchorLink
-            className= {`${selectedPage === lowerCasePage ? "text-orange" : ""}
-            hover:text-orange transition duration-500`}
+            className= {`${
+                selectedPage === lowerCasePage ? "text-orange" : ""
+            } hover:text-orange transition duration-500`}
             // Identify which section we're going to move to
             href={`#${lowerCasePage}`}
             // change useState to whichever page is selected
             onCLick={() => setSelectedPage(lowerCasePage)}
         >
             {page}
-
         </AnchorLink>
 
     )
 }
 
-const Navbar = ({selectedPage, setSelectedPage}) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}) => {
     // mobileMode: check wether menu is toggled or not
     const [isMenuToggled, setIsMenuToggled] = useState(false)
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
+    const navbarBackground = isTopOfPage ? "" : "bg-orange"
 
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className= "flex items-center justify-between mx-auto w-5/6">
                 <h4 className= "font-roboto text-3xl font-bold">JINA</h4>
                 {/* DESKTOP NAV */}
@@ -59,10 +61,46 @@ const Navbar = ({selectedPage, setSelectedPage}) => {
                         />
                     </div>
                 ) : (
-                <button>
-                    <img alt="menuIcon" src="../Assets/menuIcon.png"/>
-
+                <button className="rounded-full bg-orange-500 p-2"
+                        onCLick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <img src= "../assets/menu-icon.svg" />
                 </button>
+                )}
+                {/* Mobile menu popup */}
+                {!isAboveSmallScreens && isMenuToggled && (
+                    <div className="fixed right-0 bottom-0 h-full bg-black w-[300px]">
+                        {/* Close Icon */}
+                        <div className= "flex justify-end p-12">
+                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <img src= "../assets/close-icon.svg"/>
+                            </button>
+
+                        </div>
+                        {/* Menu Item */}
+                        <div className= "flex flex-col gap-10 ml-[33%] text-2xl text-white">
+                            <Link
+                                page="About Me"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Skills"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Projects"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Contact me"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+
+                        </div>
+                    </div>
                 )}
             </div>
         </nav>
