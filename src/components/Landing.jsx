a8362be8a58c187172fd20ead8a9c275ec206f4d
Panisara Sonkom <[psonkom342@gmail.com]>
2023-03-30 21:08:05 +1100

    fixed landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,9 +7,9 @@ const Landing = ({setSelectedPage}) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
 
     return (
-        <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap 16 py-10">
+        <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10 mt-32">
             {/* IMAGE SECTION */}
-            <div className= "md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+            <div className= "basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
                 {isAboveMediumScreens ? (
                 <div className= "relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
                     before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
